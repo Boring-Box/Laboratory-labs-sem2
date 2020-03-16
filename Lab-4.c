e0ea6f1172328f338054889c073833316a31efd9
Boring-Box <boringbox@yahoo.com>
2020-03-16 23:06:36 +0300

    Lab-4

--- Lab-4.c
+++ Lab-4.c
@@ -1,25 +1,33 @@
-/******************************************************************************
 
-*******************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
 
-void main(void)
-{	int A[6] = {1, 3, 4, 6, 8, 10};
+void main(void){
+	int A[6] = {1, 3, 4, 6, 8, 10};
 	int B[5] = {9, 8, 7, 4, 2};
 	int C[5+6]={};
     
     printf("\n");
-	for(int i=0; i<6; i++){
+	for(unsigned int i=0; i<6; i++){
 		C[i]=A[i];
 	}
-    for(int i=6+1; i<5+6; i++){
-		C[i]=B[i];
+    for(unsigned int i=6, j=0; i<5+6, j<5; i++, j++){
+		C[i]=B[j];
 	}
-
-    printf("\nC={")
-    for(int i=0; i<5+6; i++){
-		printf("%d, ", C[i]);
+/**************Logic part*****************/
+	int accum = 0;
+	for(unsigned int i=0; i<5+6; i++){
+		if(i == 5+6-1)
+			break;
+		for(unsigned int j=i+1; j<5+6; j++){
+			if(C[j]<C[i]){
+				accum = C[i];
+				C[i] = C[j];
+				C[j] = accum;
+			}
+		}
 	}
-    printf("}")
+/******************************************/
+	for(unsigned int i=0; i<5+6; i++)
+		printf("  %d", C[i]);
 }
